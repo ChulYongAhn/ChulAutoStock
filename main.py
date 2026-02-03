f0f7ed26248ad7eff6acb474a406f90664048205
ChulYong <acy0401@gmail.com>
2026-02-03 23:37:51 +0900

    TestCommit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+ChulAutoStock - 자동 주식 트레이딩 프로젝트
+"""
+
+
+def main():
+    """메인 함수"""
+    print("ChulAutoStock 시작")
+    # TODO: 여기에 자동 주식 트레이딩 로직 추가
+
+
+if __name__ == "__main__":
+    main()
